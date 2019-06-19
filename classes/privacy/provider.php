f18c6593283eda4dc4230f11868f0ce309a55f88
Guillermo Alvarez <guillermo.alvarez@blackboard.com>
2019-06-19 15:15:24 -0500

    INT-14231: Add GDPR integrace for local_dirft

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -29,14 +29,17 @@ defined('MOODLE_INTERNAL') || die();
 
 use core_privacy\local\metadata\collection;
 use core_privacy\local\request\approved_contextlist;
+use core_privacy\local\request\approved_userlist;
 use core_privacy\local\request\context;
 use core_privacy\local\request\contextlist;
 use core_privacy\local\request\transform;
+use core_privacy\local\request\userlist;
 use core_privacy\local\request\writer;
 
 class provider implements
     \core_privacy\local\metadata\provider,
-    \core_privacy\local\request\plugin\provider {
+    \core_privacy\local\request\plugin\provider,
+    \core_privacy\local\request\core_userlist_provider {
 
     /**
      * Get the list of contexts that contain user information for the specified user.
@@ -163,5 +166,43 @@ class provider implements
 
         return $collection;
 
+    }
+
+    /**
+     * Get the list of users within a specific context.
+     *
+     * @param userlist $userlist The userlist containing the list of users who have data in this context/plugin combination.
+     */
+    public static function get_users_in_context(userlist $userlist) {
+        $context = $userlist->get_context();
+
+        if (!($context instanceof \context_user)) {
+            return;
+        }
+
+        // Drift Subscriptions.
+        $sql = "SELECT userid
+                  FROM {local_drift_subscription} drift
+                 WHERE drift.userid = :userid";
+
+        $params = ['userid' => $context->instanceid];
+
+        $userlist->add_from_sql('userid', $sql, $params);
+    }
+
+    /**
+     * Delete multiple users within a single context.
+     *
+     * @param approved_userlist $userlist The approved context and user information to delete information for.
+     */
+    public static function delete_data_for_users(approved_userlist $userlist) {
+        global $DB;
+        $context = $userlist->get_context();
+
+        if (!($context instanceof \context_user)) {
+            return;
+        }
+
+        $DB->delete_records('local_drift_subscription', ['userid' => $context->instanceid]);
     }
 }
--- tests/privacy/privacy_provider_test.php
+++ tests/privacy/privacy_provider_test.php
@@ -139,4 +139,48 @@ class local_drift_privacy_provider_testcase extends provider_testcase {
         $this->assertEquals(0, $DB->count_records('local_drift_subscription', ['userid' => $user->id]));
     }
 
+    public function test_get_users_in_context() {
+        global $DB;
+
+        $user1 = $this->getDataGenerator()->create_user();
+        $user2 = $this->getDataGenerator()->create_user();
+        $this->assertEquals(0, $DB->count_records('local_drift_subscription'));
+
+        $this->mock_drift_user_subscription($user1->id);
+        $this->assertEquals(1, $DB->count_records('local_drift_subscription'));
+
+        $context1 = \context_user::instance($user1->id);
+        $userlist = new \core_privacy\local\request\userlist($context1, 'local_drift');
+
+        provider::get_users_in_context($userlist);
+        $this->assertCount(1, $userlist);
+        $expected = [$user1->id];
+        $actual = $userlist->get_userids();
+        $this->assertEquals($expected, $actual);
+
+        $context2 = \context_user::instance($user2->id);
+        $userlist = new \core_privacy\local\request\userlist($context2, 'local_drift');
+        provider::get_users_in_context($userlist);
+        $this->assertCount(0, $userlist);
+    }
+
+    public function test_delete_data_for_users() {
+        global $DB;
+
+        $user1 = $this->getDataGenerator()->create_user();
+        $user2 = $this->getDataGenerator()->create_user();
+        $this->assertEquals(0, $DB->count_records('local_drift_subscription'));
+
+        $this->mock_drift_user_subscription($user1->id);
+        $this->mock_drift_user_subscription($user2->id);
+        $this->assertEquals(2, $DB->count_records('local_drift_subscription'));
+
+        $context = \context_user::instance($user1->id);
+        $userlist = new \core_privacy\local\request\approved_userlist($context, 'local_drift', [$user1->id, $user2->id]);
+        provider::delete_data_for_users($userlist);
+
+        $this->assertEquals(0, $DB->count_records('local_drift_subscription', ['userid' => $user1->id]));
+        $this->assertEquals(1, $DB->count_records('local_drift_subscription'));
+    }
+
 }
